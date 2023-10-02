135f45de720efe18e3e95684a40e4934ca640fd7
Kuesung Park <gueit214@naver.com>
2023-10-02 09:11:53 +0900

    refactor : ts > js로 수정

--- scheduler/app.js
+++ scheduler/app.js
@@ -1,4 +1,3 @@
-//app.js
 const axios = require("axios");
 const schedule = require("node-schedule");
 const app = require("express")();
@@ -6,7 +5,6 @@ const app = require("express")();
 const SCHEDULE_PORT = 8000;
 
 app.listen(SCHEDULE_PORT, () => {
-  //매 5분마다 수행!
   const job = schedule.scheduleJob({ second: 0 }, function () {
     axios.post(`https://automation-tools-nu.vercel.app/api/slack`, {
       text: "안녕하세요 -!",
