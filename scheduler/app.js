4c33681df0dcee419beb6ef9b33804b8a055a53d
Kuesung Park <gueit214@naver.com>
2023-10-02 09:21:50 +0900

    feat : 한국 시간으로 서버 시간 설정

--- scheduler/app.js
+++ scheduler/app.js
@@ -1,11 +1,14 @@
 const axios = require("axios");
 const schedule = require("node-schedule");
 const app = require("express")();
+var moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
 
 const SCHEDULE_PORT = 8000;
 
 app.listen(SCHEDULE_PORT, () => {
-  const job = schedule.scheduleJob({ second: 0 }, function () {
+  const job = schedule.scheduleJob({ hour: 0, minute: 0, minute: 0 }, function () {
     axios.post(`https://automation-tools-nu.vercel.app/api/slack`, {
       text: "안녕하세요 -!",
     });
