0da6ca26b877792282c6356bd8ac0b351d2cdb2d
Kuesung Park <gueit214@naver.com>
2023-10-02 09:42:26 +0900

    refactor : 00시 00분 00초에 실행

--- scheduler/app.js
+++ scheduler/app.js
@@ -8,7 +8,7 @@ moment.tz.setDefault("Asia/Seoul");
 const SCHEDULE_PORT = 8000;
 
 app.listen(SCHEDULE_PORT, () => {
-  const job = schedule.scheduleJob({ hour: 0, minute: 0, minute: 0 }, function () {
+  schedule.scheduleJob({ hour: 0, minute: 0, second: 0 }, function () {
     axios.post(`https://automation-tools-nu.vercel.app/api/slack`, {
       text: "안녕하세요 -!",
     });
